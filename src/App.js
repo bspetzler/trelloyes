cc7855ca6b2632b907036d9351a26b75b927d66c
Brenden Spetzler <brendenspetzler@Brendens-iMac.local>
2020-04-10 13:22:24 -0600

    trelloyes no style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import List from './List';
+
+class App extends Component {
+  // 'class App extends Component {' if import React, { Component } from 'react'; is used
+  static defaultProps = { // creates empty object for default prop 'store'
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+
+  render() {
+    console.log(this.props);
+    const { store } = this.props // makes {store}, this will be the empty object above if no store object is passed in
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => ( //fills empty array 'list' with in empty object 'store' with List
+            <List // from List function in List.js, 
+              key={list.id} // every element must have a key, this key is the id of the element
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import STORE from './STORE';
+console.log({App})
+ //pass in store (object with all the info to manipulate) as a prop
+ReactDOM.render(<App store={STORE}/>, document.getElementById('root')); //renders App into the element with 'root' Id
+// putting store={store} in App makes the prop 'store' in App.js populate with values that trickle down to all the other sub-components
+// without this, the default store is empty
